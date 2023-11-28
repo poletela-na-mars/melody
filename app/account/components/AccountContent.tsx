de2dcc9092e93251ff27d507f7e591642b9acfe4
Polina <polina-postylyakova@yandex.ru>
2023-11-28 12:04:51 +0300

    Account page

--- app/account/components/AccountContent.tsx
+++ app/account/components/AccountContent.tsx
@@ -1,10 +1,14 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import toast from 'react-hot-toast';
 
 import useSubscribeModal from '@/hooks/useSubscribeModal';
 import { useUser } from '@/hooks/useUser';
+import { postData } from '@/libs/helpers';
+
+import Button from '@/components/Button';
 
 const AccountContent = () => {
 	const router = useRouter();
@@ -13,8 +17,53 @@ const AccountContent = () => {
 
 	const [loading, setLoading] = useState(false);
 
+	useEffect(() => {
+		if (!isLoading && !user) {
+			router.replace('/');
+		}
+	}, [isLoading, user, router]);
+
+	const redirectCustomerPortal = async () => {
+		setLoading(true);
+		try {
+			const { url, error } = await postData({
+				url: '/api/create-portal-link'
+			});
+			window.location.assign(url);
+		} catch (error) {
+			if (error) {
+				toast.error((error as Error).message);
+			}
+		}
+
+		setLoading(false);
+	};
+
 	return (
-		<div>AccountContent</div>
+		<div className='mb-7 px-6'>
+			{
+				!subscription ? (
+					<div className='flex flex-col gap-y-4'>
+						<p>No active plan</p>
+						<Button
+							onClick={subscribeModal.onOpen}
+							className='w-[300px]'
+						>
+							Subscribe
+						</Button>
+					</div>
+				) : (
+					<div className='flex flex-col gap-y-4'>
+						<p>
+							You are currently on the <b>{subscription?.prices?.products?.name}</b> plan.
+						</p>
+						<Button disabled={loading || isLoading} onClick={redirectCustomerPortal} className='w-[300px]'>
+							Open customer portal
+						</Button>
+					</div>
+				)
+			}
+		</div>
 	);
 };
 
